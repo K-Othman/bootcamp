91f9ca90121d138e829200ee80a682eda3c196b3
K-Othman <karim_othman88@hotmail.com>
2022-06-05 15:17:07 +0100

    first commit

---
+++ filterString.js
@@ -0,0 +1,16 @@
+let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,1,2,0,Z";
+
+let solution = myString
+  .split(",")
+  .filter(function (ele) {
+    return isNaN(parseInt(ele)) ? ele : "";
+  })
+  .map(function (ele) {
+    return ele.replace("_", " ");
+  })
+  .reduce(function (acc, current) {
+    return acc + current;
+  })
+  .slice(!false, -true);
+
+console.log(solution);
